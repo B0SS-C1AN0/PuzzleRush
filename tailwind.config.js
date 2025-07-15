4019a47c7b743181e7d5de45f2484fb36107e73c
BosscianO <louiscolix@gmail.com>
2025-07-15 13:21:54 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  }, minecraft: ['"Press Start 2P"', 'cursive'],
+  extend: {
+      fontFamily: {
+        minecraft: ['"Press Start 2P"', 'cursive'],
+      },
+    },
   plugins: [],
 };
