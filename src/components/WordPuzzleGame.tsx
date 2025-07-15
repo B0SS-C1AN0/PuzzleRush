54b5e931acdf057702f23cbfe38a16d9f4224636
BosscianO <louiscolix@gmail.com>
2025-07-15 07:19:53 +0100

    Updated WordPuzzleGame.tsx

--- src/components/WordPuzzleGame.tsx
+++ src/components/WordPuzzleGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo} from 'react';
 import { Lightbulb, User, Target } from 'lucide-react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import LetterWheel from './LetterWheel';
