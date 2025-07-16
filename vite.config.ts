06ab7521c5fa41f0f6a3a1664fff665d082cd6b9
BosscianO <louiscolix@gmail.com>
2025-07-16 02:22:44 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    target: 'es2022',
+    sourcemap: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
